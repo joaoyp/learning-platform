9773a4ed76a8de0e0f40d2d14a838eda1939b124
joaoyp <joaoyp.dev@gmail.com>
2023-10-21 19:50:52 +0100

    Backup (working from another machine)

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,36 +3,48 @@ import { Container, Typography, Button } from "@mui/material";
 
 export const LandingPage = () => {
   return (
-    <div className="relative">
-      <div className="bg-hero bg-cover w-full h-screen"></div>
-      <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 bg-black">
-        <div className="text-white text-4xl font-bold">
-          <Container
-            maxWidth="sm"
-            className="text-center relative text-white flex items-center"
-          >
-            <Typography variant="h2" component="h1" gutterBottom>
-              Welcome to Our Learning Platform
-            </Typography>
-            <Typography variant="h5" gutterBottom>
-              Unlock your potential with our wide range of courses.
-            </Typography>
-            <Button
-              variant="outlined"
-              href="/courses"
-              sx={{
-                borderColor: "#00b0b0",
-                color: "#00b0b0",
-                "&:hover": {
-                  borderColor: "#00b0b0",
-                },
-              }}
+    <>
+      <div id="hero" className="relative">
+        <div className="bg-hero bg-cover w-full h-screen"></div>
+        <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 bg-black">
+          <div className="text-white text-4xl font-bold">
+            <Container
+              maxWidth="sm"
+              className="text-center relative text-white flex items-center"
             >
-              Get Started
-            </Button>
-          </Container>
+              <Typography variant="h2" component="h1" gutterBottom>
+                Welcome to Our{" "}
+                <span className="text-teal">Learning Platform</span>
+              </Typography>
+              <Typography variant="h5" gutterBottom>
+                Unlock your potential with our wide range of courses.
+              </Typography>
+              <div className=" mt-8">
+                <Button
+                  variant="outlined"
+                  href="/courses"
+                  size="large"
+                  sx={{
+                    borderColor: "#ffffff",
+                    color: "#2DD4BF",
+                    "&:hover": {
+                      borderColor: "#ffffff",
+                      backgroundColor: "#008080",
+                      color: "#ffffff",
+                      opacity: "85%",
+                    },
+                  }}
+                >
+                  Get Started
+                </Button>
+              </div>
+            </Container>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div id="teal" className="h-screen bg-dark-teal "></div>
+      <div id="white" className="h-screen bg-white"></div>
+    </>
   );
 };
