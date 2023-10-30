ec24026d22814388a69d3bef7d9fb1f7c0448719
joaoyp <joaoyp.dev@gmail.com>
2023-10-30 18:11:14 +0000

    More filters

--- src/components/courses/SearchAside.jsx
+++ src/components/courses/SearchAside.jsx
@@ -9,6 +9,8 @@ import { FormControlLabel } from "@mui/material";
 import { Checkbox } from "@mui/material";
 import { Typography } from "@mui/material";
 import { Rating } from "@mui/material";
+import { Slider } from "@mui/material";
+import { TextField } from "@mui/material";
 
 export const SearchAside = () => {
   const searchBarValue = useSelector((state) => state.searchBar.value);
@@ -16,12 +18,21 @@ export const SearchAside = () => {
   const orderSearchValue = useSelector((state) => state.orderSearch.value);
 
   const [categories, setCategories] = useState([
-    "Category 1",
-    "Category 2",
-    "Category 3",
+    "Java",
+    "C#",
+    "Kotlin",
+    "C",
+    "C++",
+    "SQL",
+    "HTML",
+    "CSS",
+    "Kotlin",
   ]);
 
-  const [rating, setRating] = useState("0");
+  const [duration, setDuration] = useState(["< 1h", "> 1h and < 4h", "> 4h"]);
+
+  const [rating, setRating] = useState(3);
+  const [price, setPrice] = useState([0, 500]);
 
   return (
     <>
@@ -43,8 +54,8 @@ export const SearchAside = () => {
           />
         </div>
         <div className="m-7">
-          <p>Categories:</p>
-          <div className="m-2 w-full max-h-48 overflow-y-auto">
+          <Typography>Categories:</Typography>
+          <div className="m-2 w-full max-h-52 overflow-y-auto">
             <FormGroup>
               {categories.map((category, index) => (
                 <FormControlLabel
@@ -63,12 +74,78 @@ export const SearchAside = () => {
               ))}
             </FormGroup>
           </div>
-          <p>Price Range:</p>
-          <p>Duration:</p>
-          <Typography component="legend">Rating</Typography>
+          <Typography>Price Range in €:</Typography>
+          <Slider
+            className="m-2"
+            getAriaLabel={() => "Price Range"}
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            valueLabelDisplay="auto"
+            sx={{
+              color: "#008080",
+            }}
+            step={50}
+            min={0}
+            max={500}
+          />
+          <div className="mb-4 flex justify-evenly">
+            <TextField
+              className="w-1/5"
+              size="small"
+              id="min"
+              inputProps={{ min: 0, style: { textAlign: "center" } }}
+              placeholder="Min"
+              value={parseInt(price[0])}
+              onChange={(event) =>
+                setPrice([
+                  event.target.value === "" ? 0 : parseInt(event.target.value),
+                  price[1],
+                ])
+              }
+            />
+
+            <TextField
+              className="w-1/5"
+              inputProps={{ min: 0, style: { textAlign: "center" } }}
+              size="small"
+              id="max"
+              value={parseInt(price[1])}
+              placeholder="Max"
+              onChange={(event) =>
+                setPrice([
+                  price[0],
+                  event.target.value === ""
+                    ? 500
+                    : parseInt(event.target.value),
+                ])
+              }
+            />
+          </div>
+          <Typography>Duration:</Typography>
+          <div className="m-2 w-full max-h-32 overflow-y-auto">
+            <FormGroup>
+              {duration.map((duration, index) => (
+                <FormControlLabel
+                  key={index}
+                  control={
+                    <Checkbox
+                      sx={{
+                        "&.Mui-checked": {
+                          color: "#008080",
+                        },
+                      }}
+                    />
+                  }
+                  label={duration}
+                />
+              ))}
+            </FormGroup>
+          </div>
+          <Typography>Rating:</Typography>
           <Rating
             className="m-2"
-            name="simple-controlled"
             value={rating}
             onChange={(event, newValue) => {
               setRating(newValue);
