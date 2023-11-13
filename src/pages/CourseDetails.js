e968679d82b1d3ae8aaaafdc05b1381f71203d54
joaoyp <joaoyp.dev@gmail.com>
2023-11-13 05:12:14 +0000

    Course Details placeholders and raw layout

--- src/pages/CourseDetails.js
+++ src/pages/CourseDetails.js
@@ -1,7 +1,8 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Lettering } from "../components/common/Lettering";
+import { PrimaryButton } from "../components/common/PrimaryButton";
 
 export const CourseDetails = () => {
   const { courseId } = useParams("");
@@ -33,16 +34,44 @@ export const CourseDetails = () => {
   }, [courseId]);
 
   return (
-    <>
-      {loading ? (
-        <div></div>
-      ) : (
-        <>
-          {Object.entries(courseDetails).map(([key, value]) => (
-            <Lettering text={key + " : " + value} />
-          ))}
-        </>
+    <div className="container mx-auto mt-8">
+      {loading ? null : (
+        <div>
+          <img
+            src={courseDetails.imagePath}
+            alt={courseDetails.name}
+            className="w-full h-96 object-cover rounded-lg mb-4"
+          />
+          <h1 className="text-3xl font-bold mb-4">{courseDetails.name}</h1>
+          <p className="text-neutral-700 mb-4">{courseDetails.description}</p>
+          <p>
+            <strong>Instructor:</strong> {courseDetails.instructor}
+          </p>
+          <p>
+            <strong>Duration:</strong> {courseDetails.duration}
+          </p>
+          <p>
+            <strong>Price:</strong> ${courseDetails.price}
+          </p>
+          <p>
+            <strong>Difficulty:</strong> {courseDetails.difficulty}
+          </p>
+          <p>
+            <strong>Rating:</strong> {courseDetails.rating}
+          </p>
+          <p>
+            <strong>Category:</strong> {courseDetails.category}
+          </p>
+          <p>
+            <strong>Created At:</strong> {courseDetails.created_at}
+          </p>
+          <div className="mt-4">
+            <Link to={"/user-profile"}>
+              <PrimaryButton text={"Enroll"} />
+            </Link>
+          </div>
+        </div>
       )}
-    </>
+    </div>
   );
 };
