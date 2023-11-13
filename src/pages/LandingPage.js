47fc7865c00344dfc80df560f150a8daee9723db
joaoyp <joaoyp.dev@gmail.com>
2023-11-13 19:48:28 +0000

    Error component not found fixed

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,7 @@
 import React from "react";
-import { Container } from "@mui/material";
-import { PrimaryButton } from "../components/common/PrimaryButton";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
-import { Lettering } from "../components/common/Lettering";
-import { IntroText } from "../components/structure/landing/IntroText";
 import { Hero } from "../components/structure/landing/Hero";
 import { How } from "../components/structure/landing/How";
-import { Benefits } from "../components/structure/landing/Benefits";
-import { FeaturedCourses } from "../components/structure/landing/FeaturedCourses";
 
 export const LandingPage = () => {
   useEffect(() => {
